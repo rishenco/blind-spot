d082e2e3f4843771e6a4e675035d6e695f6910a7
rishenco <r.ishenko@icloud.com>
2026-08-22 20:18:10 +0500

    reset

--- tools/find-seed.ts
+++
@@ -1,5 +0,0 @@
-import { Room } from '../src/server/room.ts';
-for (let s = 1; s < 400; s++) {
-  const r = new Room('SEED', s);
-  if (r.relicSite === 'VAULT') { console.log('seed', s, '-> relic at', r.relicSite); break; }
-}
